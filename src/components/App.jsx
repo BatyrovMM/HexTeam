c897705690302c11893c53805a1e63e3724451c2
Malik Batyrov <batyrov.m.m@yandex.ru>
2022-09-02 12:16:13 +0300

    Api.js complete

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import { Api } from "../utils/api";
+
+function App() {
+  const api = new Api({
+    baseUrl: 'http://79.143.31.216'
+  });
+
+  return (
+    <div className="page">
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utils/api.js
@@ -0,0 +1,72 @@
+export class Api {
+  constructor(options) {
+    this.baseUrl = options.baseUrl;
+  }
+
+  _response(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`error${res.status}`);
+  }
+
+  // _responseUrl(res) {
+  //   if (res.ok) {
+  //     return res;
+  //   }
+  //   return Promise.reject(`error${res.status}`);
+  // }
+
+  login(username, password) {
+    return fetch(`${this.baseUrl}/login`, {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body: `grant_type=&username=${username}&password=${password}&scope=&client_id=&client_secret=`,
+      method: "POST",
+    })
+    .then(this._response);
+  }
+
+  register(username, password) {
+    return fetch(`${this.baseUrl}/register?username=${username}&password=${password}`, {
+      headers: {
+        Accept: "application/json"
+      },
+      method: "POST"
+    })
+    .then(this._response);
+  }
+
+  squeeze(link, jwt) {
+    return fetch(`${this.baseUrl}/squeeze?link=${link}`, {
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${jwt}`
+      },
+      method: "POST"
+    })
+    .then(this._response);
+  }
+
+  statistics(order, offset, limit, jwt) {
+    return fetch(`${this.baseUrl}/statistics?order=${order}&offset=${offset}&limit=${limit}`, {
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${jwt}`
+      },
+      method: "POST"
+    })
+    .then(this._response);
+  }
+
+  // redirect(key) {
+  //   return fetch(`${this.baseUrl}/s/${key}`, {
+  //     headers: {
+  //       Accept: "*/*"
+  //     }
+  //   })
+  //   .then(this._responseUrl);
+  // }
+}
